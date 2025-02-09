46e90993275ac42f3681d16488f57c76d25f5b22
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2025-02-09 16:42:25 +0530

    Demo for IPv4 & IPv6

--- main.c
+++ main.c
@@ -66,6 +66,20 @@ struct sockaddr_storage
 };
 
 int main() {
-    // code..
+    // IPv4 :
+
+    char ip4[INET_ADDRSTRLEN]; // space to hold the IPv4 string
+    struct sockaddr_in sa; // pretend this is loaded with something
+
+    inet_ntop(AF_INET, &(sa.sin_addr), ip4, INET_ADDRSTRLEN);
+    printf("The IPv4 address is: %s\n", ip4);
+
+    // IPv6 :
+
+    char ip6[INET_ADDRSTRLEN]; // space to hold the IPv6 string
+    struct sockaddr_in6 sa6; // pretend this is loaded with something
+
+    inet_ntp(AF_INET6, &(sa6.sin6_addr), ip6, INET6_ADDRSTRLEN);
+    printf("The IPv6 address is: %s\n", ip6);
     return 0;
 }
